123010bd178b76b4516944bb1a9023764c561de5
shila <shila@novainternationaldesigns.com>
2026-01-06 16:38:45 -0600

    did changes in cors in the server.js file

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ connectDB();
 // Middleware
 // app.use(cors({ origin: "*", methods: ["GET","POST","PUT","DELETE"] }));
 
-app.use(cors({origin: ["http://localhost:5173","https://nova-international-test.netlify.app"]}));
+app.use(cors({origin: "*"})); // Allow all origins
 
 app.use(express.json());
 
